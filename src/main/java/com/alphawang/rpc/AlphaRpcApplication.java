cc04233337503ffda4222f4ed1a2d8703f4b2b71
AlphaWang <1919wang@gmail.com>
2018-07-28 23:19:17 +0800

    [RPC] init

---
+++ src/main/java/com/alphawang/rpc/AlphaRpcApplication.java
@@ -0,0 +1,11 @@
+package com.alphawang.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class AlphaRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaRpcApplication.class, args);
+    }
+}
