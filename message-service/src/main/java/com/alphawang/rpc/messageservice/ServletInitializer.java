5f500b58ab8bcf1917860b7211652f28d36ae2c0
AlphaWang <1919wang@gmail.com>
2018-07-29 19:01:38 +0800

    [RPC] message-service

---
+++ message-service/src/main/java/com/alphawang/rpc/messageservice/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.alphawang.rpc.messageservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MessageServiceApplication.class);
+    }
+
+}
