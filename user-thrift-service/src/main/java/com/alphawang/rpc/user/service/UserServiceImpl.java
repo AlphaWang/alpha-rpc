1933d2c491c8a3775bdd0fb78531d2d0b705a5d9
Alpha <awang@coupang.com>
2018-08-02 22:58:32 +0800

    [RPC] user service.

--- user-thrift-service/src/main/java/com/alphawang/rpc/user/service/UserServiceImpl.java
+++ user-thrift-service/src/main/java/com/alphawang/rpc/user/service/UserServiceImpl.java
@@ -1,22 +1,31 @@
 package com.alphawang.rpc.user.service;
 
-import com.alphawang.rpc.thrift.user.UserInfo;
-import com.alphawang.rpc.thrift.user.UserService;
+import com.alphawang.rpc.thrift.user.service.api.UserInfo;
+import com.alphawang.rpc.thrift.user.service.api.UserService;
+import com.alphawang.rpc.user.mapper.UserMapper;
 import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService.Iface {
+    
+    @Autowired
+    private UserMapper userMapper;
+    
+    
     @Override 
     public UserInfo getUserById(int id) throws TException {
-        return null;
+        return userMapper.getUserById(id);
     }
 
     @Override 
     public UserInfo getUserByName(String username) throws TException {
-        return null;
+        return userMapper.getUserByName(username);
     }
 
     @Override 
     public void registerUser(UserInfo userInfo) throws TException {
-
+        userMapper.registerUser(userInfo);
     }
 }
