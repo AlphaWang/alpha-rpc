3cac804243c02adc5cad95560fc9034c4c72e347
AlphaWang <1919wang@gmail.com>
2018-08-12 15:02:55 +0800

    [RPC] fix typo addredd

--- course-edge-service/src/main/java/com/alphawang/rpc/course/edge/controller/CourseController.java
+++ course-edge-service/src/main/java/com/alphawang/rpc/course/edge/controller/CourseController.java
@@ -7,7 +7,6 @@ import com.alphawang.rpc.thrift.user.service.api.dto.UserDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
