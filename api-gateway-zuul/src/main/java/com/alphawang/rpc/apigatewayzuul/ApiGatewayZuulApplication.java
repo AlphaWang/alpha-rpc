0a87ec58087e5ad357704f7e8060696ccdf4d9e0
AlphaWang <1919wang@gmail.com>
2018-08-14 23:08:35 +0800

    [RPC] API Gateway Zuul

---
+++ api-gateway-zuul/src/main/java/com/alphawang/rpc/apigatewayzuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,14 @@
+package com.alphawang.rpc.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication 
+@EnableZuulProxy
+public class ApiGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayZuulApplication.class, args);
+    }
+}
