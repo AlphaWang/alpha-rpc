852f578bdf7857e25758982c0ae2f0e93cb5a87c
AlphaWang <1919wang@gmail.com>
2018-08-23 23:45:19 +0800

    [restTemplate] LoadBalancerClient.choose

---
+++ api-gateway-zuul/src/main/java/com/alphawang/rpc/apigatewayzuul/controller/LoadBalanceController.java
@@ -0,0 +1,30 @@
+package com.alphawang.rpc.apigatewayzuul.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
+public class LoadBalanceController {
+    
+    private static String PATH = "/course/list";
+    
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    
+    @RequestMapping("loadbalance")
+    public String loadBalance() {
+        RestTemplate restTemplate = new RestTemplate(); 
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("course-edge-service");
+        String url = String.format("http://%s:%s/%s", serviceInstance.getHost(), serviceInstance.getPort(), PATH);
+        String response = restTemplate.getForObject(url, String.class);
+        
+        return response;
+    }
+}
